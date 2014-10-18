a7b41a8f20517fcf350d433736dbdee9b6c42ed2
gearsgrinding <hualex@seas.upenn.edu>
2014-10-18 15:02:45 -0400

    Create test.m

---
+++ test.m
@@ -0,0 +1,14 @@
+x1=input('x1=');
+x2=input('x2=');
+y1=input('y1=');
+y2=input('y2=');
+w=input('weight=');
+theta1=atand(y1/x1);
+theta2=atand(y2/x2);
+matrix=[sind(theta1) sind(theta2); cosd(theta1) -cosd(theta2)];
+values=[w;0];
+solution=linsolve(matrix,values);
+disp('t1=');
+disp(solution(1,1));
+disp('t2=');
+disp(solution(2,1));

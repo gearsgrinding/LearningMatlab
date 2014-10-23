f2889c439d31806b19accf85d7ee4babeb4b1102
gearsgrinding <hualex@seas.upenn.edu>
2014-10-23 09:30:37 -0400

    asdf part 2

--- cost.m
+++ cost.m
@@ -3,13 +3,13 @@ Volume=input('volume');
 cables=input('cables');
 hangers=input('hangers');
 deck_truss=input('maximum distance of hangers');
-if location==6
+if location==6 %Cost based on location 6 or 8 feet%
 x=50;
 else 
 x=10;
 end
-x=x+volume*0.4;
-x=x+(numlib:fibonacci(cables))*10;
-x=x+(5*hangers);
-x=x+(deck_truss)^2/40;
-x
+x=x+volume*0.4; %Adding cost based on volume in cm^2%
+x=x+(numlib:fibonacci(cables+1))*10; %Adding cost based on the fibonacci sequence for cables%
+x=x+(5*hangers); %Adding cost of hangers (5Mcu each)%
+x=x+(deck_truss)^2/40; %Addinc cost for maximum distance of hangers%
+x %output of the final sum%
--- updatedtest.m
+++ updatedtest.m
@@ -2,26 +2,27 @@ hangers=input('hangers ');
 mass=input('mass ');
 height=input('height of pylons ');
 h=input('height from the road ');
-sec=ceil(hangers/2);
-theta1=atand(((height-h)/sec)/(36/(hangers+1)));
-w=9.8*mass;
+sec=ceil(hangers/2); %Finding number of height divisions%
+theta1=atand(((height-h)/sec)/(36/(hangers+1))); %Finding the angles from the height divisions%
+w=9.8*mass;%converting mass to weight%
 
-matrix=[sind(theta1) sind(theta1); cosd(theta1) -cosd(theta1)];
-if mod(hangers,2)==0
-for x=1:1:sec
-    if x==sec
+matrix=[sind(theta1) sind(theta1); cosd(theta1) -cosd(theta1)]; %creating a matrix of x and y forces%
+
+if mod(hangers,2)==0 %Case of even number of hangers%
+for x=1:1:sec %find all tension members going from most to least%
+    if x==sec %Accounting for even number of hangers%
     values=[(2*w/x);0];
     else
     values=[(w/x);0];
     end
-solution=linsolve(matrix,values);
-disp('t1=');
+solution=linsolve(matrix,values);%getting values of t1 and t2%
+disp('t1='); %printing values%
 disp(solution(1,1));
 disp('t2=');
 disp(solution(2,1));
 end
-else 
-for x=1:1:sec
+else %Case of odd number of hangers%
+for x=1:1:sec %find all tension members going from most to least% 
 values=[(w/x);0];
 solution=linsolve(matrix,values);
 disp('t1=');
